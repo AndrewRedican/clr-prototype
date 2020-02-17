25bc60d63b7cd89c1f4320e1ef44bd337800b1a2
andrew_redicon <andrew.redicon@dellteam.com>
2020-02-17 10:44:22 +0000

    angular 9 project upgrade

--- clr-prototype-angular/src/app/app.component.spec.ts
+++ clr-prototype-angular/src/app/app.component.spec.ts
@@ -16,20 +16,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'clr-prototype-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app.title).toEqual('clr-prototype-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
+    const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('clr-prototype-angular app is running!');
   });
 });
