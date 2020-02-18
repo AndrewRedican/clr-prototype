b446c22a940d01f1d3618a1803ffa4a823a70c7a
andrew_redicon <andrew.redicon@dellteam.com>
2020-02-18 13:24:55 +0000

    Fixed Lint Conflicting Editor Setting on Angular

--- clr-prototype-angular/src/app/app.module.ts
+++ clr-prototype-angular/src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ClarityModule } from "@clr/angular";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 
 @NgModule({
   declarations: [AppComponent],
